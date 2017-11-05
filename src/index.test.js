468528610db90f19a0e544b692f40d3bdd3a3118
Yoann Pigné <yoann.pigne@univ-lehavre.fr>
2017-11-05 11:51:35 +0100

    Refactoring and simplifications

---
+++ src/index.test.js
@@ -0,0 +1,17 @@
+
+import { version } from ".";
+import  { data }  from '../resources/sensors_data';
+
+describe('Sensor model tests', () => {
+  describe('Dummy tests', () => {
+    test('data is loaded with 3 elements',() => {
+      expect(data.length).toBe(3);
+    });
+    test('version number from the model',() => {
+      expect(version()).toBe('1.0.0');
+    });
+  });
+  /* TODO: Écrire ici la suite de tests pour le modèle objet.*/
+  //
+  console.log("qdsf");
+});
--- src/model/index.js
+++
@@ -1,2 +0,0 @@
-
-/* TODO : Créer le modèle objet ici */ 
